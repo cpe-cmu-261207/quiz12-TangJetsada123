ff8c3ee66fa2ecac228d736f2bb1e92c50047b74
เจษฎา จินะกะ <Jetsada_Jinaka@hotmail.com>
2021-01-12 14:07:13 +0700

    ver1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,14 @@
-//your code here!
+document.addEventListener('DOMContentLoaded', function() {
+    function update(){
+        const Name = document.querySelector('#input-number').value
+       const div = document.createElement("div")
+        const body = document.body
+        div.innerHTML = "Create by Jetsada Jinaka 620612144"
+        
+    document.querySelector('p').innerHTML = 62061214 + Name 
+        body.append(div)
+    }
+
+    document.querySelector('#input-number').onkeyup = update
+    
+})
